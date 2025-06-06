ae6031dec07db1cb0979837209b074f7caa692da
Ad-SilvaS <diegosilvaa63@gmail.com>
2025-06-06 17:57:49 -0300

    EmployeeDTO

---
+++ src/main/java/com/dgg/project/DTO/EmployeeDTO.java
@@ -0,0 +1,74 @@
+package com.dgg.project.DTO;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class EmployeeDTO {
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String phone;
+    @NotNull
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate birthDate;
+    @NotNull
+    private Integer departmentId;
+
+    public EmployeeDTO() {
+    }
+
+    public EmployeeDTO(@NotBlank String name, @NotBlank String email, @NotBlank String phone,
+            @NotNull LocalDate birthDate, @NotNull Integer departmentId) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.departmentId = departmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+}
