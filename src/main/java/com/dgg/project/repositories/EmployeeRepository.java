5a7f0d13adedb4ec5e512ca298a55db31c1756bb
Ad-SilvaS <diegosilvaa63@gmail.com>
2025-06-06 18:11:00 -0300

    Employee Repository

---
+++ src/main/java/com/dgg/project/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.dgg.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dgg.project.entities.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    
+}
