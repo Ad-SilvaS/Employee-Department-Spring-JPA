786b372a97cf89149cb47e9fd3c7aea45eb7f6be
Ad-SilvaS <diegosilvaa63@gmail.com>
2025-06-06 18:25:42 -0300

    Convert Employee to EmployeeDTO method

--- src/main/java/com/dgg/project/services/EmployeeService.java
+++ src/main/java/com/dgg/project/services/EmployeeService.java
@@ -2,14 +2,27 @@ package com.dgg.project.services;
 
 import org.springframework.stereotype.Service;
 
+import com.dgg.project.DTO.EmployeeDTO;
+import com.dgg.project.entities.Employee;
+import com.dgg.project.repositories.DepartmentRepository;
 import com.dgg.project.repositories.EmployeeRepository;
 
 @Service
 public class EmployeeService {
     private EmployeeRepository empRepo;
+    private DepartmentRepository depRepo;
 
-    public EmployeeService(EmployeeRepository empRepo) {
+    public EmployeeService(EmployeeRepository empRepo, DepartmentRepository depRepo) {
         this.empRepo = empRepo;
+        this.depRepo = depRepo;
     }
 
+    public EmployeeDTO convertToDTO(Employee emp) {
+        return new EmployeeDTO(
+                emp.getName(),
+                emp.getEmail(),
+                emp.getPhone(),
+                emp.getBirthDate(),
+                emp.getDepartment().getId());
+    }
 }
